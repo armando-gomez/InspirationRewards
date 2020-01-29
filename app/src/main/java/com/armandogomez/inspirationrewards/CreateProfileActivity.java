5b77ec8529e29166345f4b4108c52e4e0dc521a2
Armando Gomez <armando.0196@gmail.com>
2020-01-28 18:09:32 -0600

    adding more work

---
+++ app/src/main/java/com/armandogomez/inspirationrewards/CreateProfileActivity.java
@@ -0,0 +1,16 @@
+package com.armandogomez.inspirationrewards;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class CreateProfileActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_create_profile);
+	}
+}
--- app/src/main/java/com/armandogomez/inspirationrewards/MainActivity.java
+++ app/src/main/java/com/armandogomez/inspirationrewards/MainActivity.java
@@ -1,5 +1,6 @@
 package com.armandogomez.inspirationrewards;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,45 +12,36 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+	TextView createAccount;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
 		Toolbar toolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
+		getSupportActionBar().setDisplayShowHomeEnabled(true);
+		getSupportActionBar().setLogo(R.drawable.icon);
+		getSupportActionBar().setDisplayUseLogoEnabled(true);
 
-		FloatingActionButton fab = findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
+		createAccount = this.findViewById(R.id.loginCreateAccount);
+
+		createAccount.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
+			public void onClick(View v) {
+				openCreateProfileActivity();
 			}
 		});
+
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
+	public void openCreateProfileActivity() {
+		Intent intent = new Intent(this, CreateProfileActivity.class);
+		startActivity(intent);
 	}
 }
